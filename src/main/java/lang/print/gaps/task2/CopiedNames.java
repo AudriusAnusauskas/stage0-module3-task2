073ed6a171432018841af5d521cd0be1a5ec78d3
AudriusAnusauskas <aaaudrius@gmail.com>
2023-05-26 11:17:16 +0300

    correct copied names

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -7,8 +7,12 @@ public class CopiedNames {
         int value = 1000;
         int variable2 = 4000;
         int value2 = 3000;
-        int variable4 = 5000;
-        variable4 = 7000;
+        int variables = 5000;
+        variables = 7000;
         int value3 = 90000;
+        int integer = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int myInteger2 = 1234567;
     }
 }
